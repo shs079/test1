637a3c133eca0c4bbb0845e78cb1cfea9bcdaba7
shs079 <148479848+shs079@users.noreply.github.com>
2023-10-19 17:38:16 +0000

    random number below 1

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    ArrayList<Double> myList = new ArrayList<>();
+
+    for (int i = 0; i < 4; i++){
+      myList.add(Math.random());
+    }
+
+    System.out.println(myList);
+  }
+public class RandomIntegerGenerator {
+    public static int generateRandomInt(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
+    }
   }
 }
+
+  
